08871e4b9803d7dcef6bdc50ff0178336d68fd9e
lu-papagni <papagniluca4@gmail.com>
2024-09-08 15:28:51 +0200

    Aggiunto script di installazione

---
+++ setup.ps1
@@ -0,0 +1,62 @@
+function Import-MySettings {
+  $programs = @{
+    powershell = @(
+      @{
+        fileName = "Microsoft.PowerShell_profile.ps1"
+        configPath = "$env:userprofile\Documents\WindowsPowerShell"
+      }
+    )
+    afterburner = @(
+      @{
+        fileName = "*.cfg"
+        configPath = "${env:ProgramFiles(x86)}\MSI Afterburner"
+      }
+    )
+    winget = @(
+      @{
+        fileName = "settings.json"
+        configPath = "$env:localappdata\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState"
+      }
+    )
+    "windows-terminal" = @(
+      @{
+        fileName = "settings.json"
+        configPath = "$env:localappdata\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+      }
+    )
+  }
+
+  foreach ($program in $programs.keys) {
+    if (Test-Path -Path "$program" -PathType Container) {
+      $targetList = $programs["$program"]
+
+      foreach ($target in $targetList) {
+        $fileName = $target.fileName
+        $configPath = $target.configPath
+
+        Copy-Item "$program\$fileName" "$configPath"
+      }
+
+    } else {
+      Write-Host -ForegroundColor Red "Impossibile trovare le impostazioni di '$program'"
+    }
+  }
+}
+
+# aggiorna tutti i pacchetti già presenti
+winget upgrade --all --accept-package-agreements
+
+# installa i pacchetti mancanti
+if (Test-Path -Path "winget\winget.json" -PathType Leaf) {
+  winget import -i "winget\winget.json" --accept-package-agreements
+} else {
+  Write-Host -ForegroundColor Red "Impossibile trovare il backup di winget"
+}
+
+# copia i file di configurazione
+Import-MySettings
+
+# cancella la repository
+$repo = "$(Get-Location)"
+cd ..
+Remove-Item -Recurse -Force "$repo"
